46e21b0e99bb76c1e84f1753edcbec17af1529c3
GianFTZ <gianfelipetz@hotmail.com>
2023-03-07 17:11:18 +0000

    refac: better imports

---
+++ src/data/contracts/index.ts
@@ -0,0 +1,2 @@
+export * from './cache-todo-repository'
+export * from './load-todo-repository'
--- src/data/services/todo-loader.ts
+++ src/data/services/todo-loader.ts
@@ -1,7 +1,6 @@
 import { Todo } from "src/domain/entities";
 import { ITodoLoader } from "src/domain/use-cases/todo-loader";
-import { ICacheTodoRepository } from "../contracts/cache-todo-repository";
-import { ILoadTodoRepository } from "../contracts/load-todo-repository";
+import { ICacheTodoRepository, ILoadTodoRepository } from "../contracts";
 
 export class TodoLoaderService implements ITodoLoader {
   constructor(

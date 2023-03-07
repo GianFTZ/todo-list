48d246012c3e539edd4e1e6863a11e12625d9fa5
GianFTZ <gianfelipetz@hotmail.com>
2023-03-07 17:21:57 +0000

    test: ensure return cache todos if have one

---
+++ src/data/services/todo-loader.spec.ts
@@ -0,0 +1,34 @@
+import { Todo } from "src/domain/entities/todo"
+import { ILoadTodoRepository, ICacheTodoRepository } from "../contracts"
+import { TodoLoaderService } from "./todo-loader"
+
+const mocks = () => {
+  class LoadTodoRepositoryMock implements ILoadTodoRepository {
+    async loadTodo(): Promise<Todo[]> {
+      return new Promise<Todo[]>((resolve) => {
+        resolve([{ title: 'test', createAt: new Date, longDesc: 'valid_long_desc', shortDesc: 'valid_short_desc' ,deleteAt: new Date(), updateAt: new Date() }])
+      })
+    }
+  }
+  class CacheTodoRepositoryMock implements ICacheTodoRepository {
+    async cache (): Promise<Todo[] | null> {
+      return new Promise<Todo[]>((resolve) => {
+        resolve([{ title: 'cache_test', createAt: new Date, longDesc: 'valid_long_desc', shortDesc: 'valid_short_desc' ,deleteAt: new Date(), updateAt: new Date() }])
+      })
+    }
+  }
+  return {
+    loadTodoRepository: new LoadTodoRepositoryMock(),
+    cacheTodoRepository: new CacheTodoRepositoryMock()
+  }
+}
+
+describe('todo loader service', ()=> {
+  test('should return cache if it had', async () => {
+    const { cacheTodoRepository, loadTodoRepository } = mocks()
+    const sut = new TodoLoaderService(loadTodoRepository, cacheTodoRepository)
+    const response = await sut.load()
+    expect(response[0].title).toBe('cache_test')
+  })
+  
+})

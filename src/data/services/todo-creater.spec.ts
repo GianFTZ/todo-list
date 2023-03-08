18d1bf7fa532d5e18b60ab0b29653ca221e240b6
GianFTZ <gianfelipetz@hotmail.com>
2023-03-08 15:23:31 +0000

    test: ensure call create todo repository

---
+++ src/data/services/todo-creater.spec.ts
@@ -0,0 +1,30 @@
+import { ICreateTodoRepository } from "../contracts/create-todo-repository"
+import { TodoModel } from "../models/todo"
+import { TodoCreateService } from "./todo-create"
+
+const mocks = () => {
+  class CreateTodoRepositoryMock implements ICreateTodoRepository {
+    async create(todo: TodoModel): Promise<TodoModel> {
+      return new Promise<TodoModel>((resolve) => {
+        resolve(todo)
+      })
+    }
+  }
+  return {
+    createTodoRepositoryMock: new CreateTodoRepositoryMock()
+  }
+}
+
+
+describe('todo create service', ()=> {
+  test('should call create todo repository', async () => {
+    const fakeData: TodoModel = { title: 'test', createAt: new Date, longDesc: 'valid_long_desc', shortDesc: 'valid_short_desc' ,deleteAt: new Date(), updateAt: new Date() }
+    const { createTodoRepositoryMock } = mocks()
+    const sut = new TodoCreateService(createTodoRepositoryMock)
+    const spy = jest.spyOn(createTodoRepositoryMock, 'create')
+    await sut.create(fakeData)
+    expect(spy).toHaveBeenCalled()
+  })
+
+  
+})

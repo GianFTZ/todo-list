5eeda3853e21ccc4bd85aaaba312711ff71c3bf5
GianFTZ <gianfelipetz@hotmail.com>
2023-03-10 17:38:49 +0000

    test: ensure return 400 if any required fields is not provided

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -32,4 +32,26 @@ describe('signup controller', () => {
     const response = await sut.handle(fakeData)
     expect(response.status).toBe(400)
   })
+  test('should return 400 if no username is provided', async () => {
+    const fakeData = {
+      body: {
+        password: 'valid_password',
+        name: 'valid_name',
+      }
+    }
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeData)
+    expect(response.status).toBe(400)
+  })
+  test('should return 400 if no name is provided', async () => {
+    const fakeData = {
+      body: {
+        username: 'valid_username',
+        password: 'valid_password',
+      }
+    }
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeData)
+    expect(response.status).toBe(400)
+  })
 })

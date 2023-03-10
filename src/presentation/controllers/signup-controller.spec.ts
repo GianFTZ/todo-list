1e36d6fda5a7f27122c525e3bc75d2d053582e79
GianFTZ <gianfelipetz@hotmail.com>
2023-03-10 18:01:15 +0000

    test: ensure controller call repository with right values

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -1,9 +1,28 @@
+import { UserCreateDto } from "src/infra/adapters/sql/prisma/protocols/user-create-dto"
 import { SignupController } from "./signup-controller"
+import { IUserCreaterRepository } from "src/data/contracts/create-user-repository";
+import { UserModel } from "src/data/models/user";
+
+
+const makeMock = () => {
+  class CreateUserRepositoryMock implements IUserCreaterRepository {
+    async create(user: UserCreateDto): Promise<UserModel | Error> {
+      return {
+        name: user.name
+      }
+    }
+  }
+  return {
+    createUserRepository: new CreateUserRepositoryMock()
+  }
+}
 
 const makeSut = () => {
-  const sut = new SignupController()
+  const { createUserRepository } = makeMock()
+  const sut = new SignupController(createUserRepository)
   return {
-    sut
+    sut,
+    createUserRepository
   }
 }
 
@@ -53,5 +72,44 @@ describe('signup controller', () => {
     const { sut } = makeSut()
     const response = await sut.handle(fakeData)
     expect(response.status).toBe(400)
+  })
+
+  test('should return 400 if no name is provided', async () => {
+    const fakeData = {
+      body: {
+        username: 'valid_username',
+        password: 'valid_password',
+      }
+    }
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeData)
+    expect(response.status).toBe(400)
+  })
+
+  test('should call repository with correct values', async () => {
+    const fakeData = {
+      body: {
+        name: 'valid_name',
+        username: 'valid_username',
+        password: 'valid_password',
+      }
+    }
+    const { sut, createUserRepository } = makeSut()
+    const spy = jest.spyOn(createUserRepository, 'create')
+    await sut.handle(fakeData)
+    expect(spy).toHaveBeenCalledWith(fakeData.body)
+  })
+
+  test('should return name if everything is okay by creating new user', async () => {
+    const fakeData = {  
+      body: {
+        name: 'valid_name',
+        username: 'valid_username',
+        password: 'valid_password',
+      }
+    }
+    const { sut } = makeSut()
+    const response = await sut.handle(fakeData)
+    expect(response.body.name).toBe(fakeData.body.name)
   })
 })

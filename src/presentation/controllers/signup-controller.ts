03d54560f2f1428eee9c9fc92c78ebfe5cf1495a
GianFTZ <gianfelipetz@hotmail.com>
2023-03-10 17:36:08 +0000

    feat: return 400 if any required fields is missing

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -11,6 +11,15 @@ import { Http } from "./contracts/http";
 
 export class SignupController implements IController {
   async handle(req: Http.Request): Promise<Http.Response> {
+    const requiredFields = ["username", "name", "password"]
+    for(const fields of requiredFields){
+      if(!req.body[fields]){
+        return {
+          body: 'missing properties',
+          status: 400
+        }
+      }
+    }
     return {
       body: req,
       status: 200
